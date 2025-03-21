967f08e5f9b0ed36240e339b72663cda942eb419
Guilhermee19 <guilherme.santana1998@gmail.com>
2025-03-21 17:20:36 -0300

    feat: reformular a página de perfil com novo layout e componentes

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
+import { AvatarComponent } from '../../shared/components/avatar/avatar.component';
+import { IconDirective } from '../../shared/directives/icon.directive';
+import { StorageService } from '../../shared/services/storage.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, MatButtonModule, AvatarComponent, IconDirective, MatMenuModule],
   templateUrl: './profile.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileComponent {
+  private storage = inject(StorageService);
 
+  public user = this.storage.myself;
+
+  // public view
 }
