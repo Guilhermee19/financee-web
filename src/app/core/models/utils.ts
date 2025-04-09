caa185199abb2268eaa8f267241bbb10db5c5378
Guilhermee19 <guilherme@noclaftech.com>
2025-04-08 23:36:26 -0300

    feat: adicionar suporte à ordenação de transações e novos filtros na interface

--- src/app/core/models/utils.ts
+++ src/app/core/models/utils.ts
@@ -1,3 +1,5 @@
+import { TRecurrence, TType } from "./finance";
+
 export interface IPagedReq<T> {
   results: T[];
   count: number;
@@ -10,7 +12,17 @@ export interface IFilter {
   page?: number;
   year: number;
   month: number;
+  account?: number;
+  category?: number;
+  status?: boolean;
+  type?: TType;
+  recurrence?: TRecurrence;
+  order_by?: string;
+  order_direction?: string;
 }
+
+export type TOrderBy = 'description' | 'account' | 'category' | 'value_installment' | 'is_paid' | 'expiry_date'
+
 
 export interface IDialogActions {
   action: 'yes' | 'no';
