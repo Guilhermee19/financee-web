d12508058f3ac54bf76b764ce51ff7e84805dfdf
Guilhermee19 <guilherme@noclaftech.com>
2025-04-23 11:28:38 -0300

    refactor: remover componente de configurações e ajustar rotas

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,11 +1,10 @@
-import { isPlatformServer } from '@angular/common';
-import { inject, PLATFORM_ID } from '@angular/core';
+import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { StorageService } from '../../shared/services/storage.service';
 
 export const authGuard: CanActivateFn = () => {
-  const platformId = inject(PLATFORM_ID);
-  if (isPlatformServer(platformId)) return true;
+  // const platformId = inject(PLATFORM_ID);
+  // if (isPlatformServer(platformId)) return true;
 
   const storage = inject(StorageService);
   const router = inject(Router);
