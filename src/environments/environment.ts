80be40a5accdc44ed774c5142a73f20a719ad51d
Guilhermee19 <guilherme@noclaftech.com>
2025-05-15 00:20:15 -0300

    chore(env): update enviroment versions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // base_url: 'https://finance-api.iamgui.dev/',
   base_url: 'http://127.0.0.1:8000/',
-  version: '1.14.3',
+  version: '1.14.4',
 };
